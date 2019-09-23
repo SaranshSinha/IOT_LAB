a8969d8207c5fab2bd34c6d4b2549d07a6664419
SaranshSinha <31996102+SaranshSinha@users.noreply.github.com>
2019-09-23 15:08:22 +0530

    Add files via upload

---
+++ pir_uno.ino
@@ -0,0 +1,15 @@
+int p=13;
+int pir=2;
+void setup()
+{
+  pinMode(p,OUTPUT);
+  pinMode(pir,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  int v=digitalRead(pir);
+  digitalWrite(p,v);
+  Serial.println(v);
+}
+
